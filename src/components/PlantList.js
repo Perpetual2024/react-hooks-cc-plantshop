e9bc1550d424f9ed167365e452b2f7c8fbfbea97
Perpetual Akinyi <you@example.com>
2024-11-11 14:02:02 +0300

    using a useeffect hook to fetch data

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,7 +1,11 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList({plants, setPlants, searchPlants}) {
+
+  useEffect(()=>{
+    
+  })
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
