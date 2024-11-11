82785948cccddbdde0c8ba28e226f48180fa5b6c
Perpetual Akinyi <you@example.com>
2024-11-11 13:42:27 +0300

    put the handleSubmit on Form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -22,7 +22,7 @@ function handleSubmit(event) {
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form >
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" onChange={handleChange}/>
         <input type="text" name="image" placeholder="Image URL" onChande={handleChange}/>
         <input type="number" name="price" step="0.01" placeholder="Price" onChange={handleChange}/>
