f6506c12c5cd9e60bec147920f0b9578fc4c3808
Perpetual Akinyi <you@example.com>
2024-11-11 14:05:17 +0300

    fetched data from my base url that has all the info

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
 import PlantCard from "./PlantCard";
-
+const baseUrl = "http://localhost:6001/plants"
 function PlantList({plants, setPlants, searchPlants}) {
 
   useEffect(()=>{
-    
+    fetch(baseUrl)
   })
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
