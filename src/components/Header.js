f0dd212b07b4ae8a8279c07aa7a1eee62d91e633
Perpetual Akinyi <you@example.com>
2024-11-11 13:39:04 +0300

    added an onchange event

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <h1>
-        Plantsy
+        Plantsy 
         <span className="logo" role="img">
           🌱
         </span>
--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React ,{useState}from "react";
+
 
 function NewPlantForm() {
+const [formData, setFormData] = useState({name:"", image: "", price: 0 });
+
+  function handleChange(event){
+    setFormData({...formData, [event.target.name]: event.target.value});
+
+  }
+
+/*function handleSubmit(event) {
+  event.preventDefault();
+
+  const newPlant={
+    name: formData.name,
+    image: formData.image,
+    price: formData.price.toString()
+  }*/
+}
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form >
+        <input type="text" name="name" placeholder="Plant name" onChange={handleChange}/>
+        <input type="text" name="image" placeholder="Image URL" onChande={handleChange}/>
+        <input type="number" name="price" step="0.01" placeholder="Price" onChange={handleChange}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
